ac8216aeb34e378bd91b1b7cce9d24002c93d3be
cantuananh <tuananhthemen391997@gmail.com>
2025-07-10 09:58:30 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Deploy dự án React jS với Cercel miễn phí!</h1>
+    </>
+  )
+}
+
+export default App
